5b655977fd1735b94b6d58d42c8fc6cd40de766e
utkarsh-1602 <usking619@gmail.com>
2024-01-25 19:17:13 +0530

    Q3_optimal completed

--- Arrays/Q3_optimal.java
+++ Arrays/Q3_optimal.java
@@ -19,10 +19,13 @@
     Approach : optimal 
 
         1. find the break point: find the number which is less than i+1, such as arr[i] < arr[i+1]
+        2. After finding the break point, try put the number which is greater than i, but the most smallest than other elements 
+        3. after you put the number, sort the remaining elements in ascending order 
 
-    Time Complexity:
+    Time Complexity: O(3N), where N = size of the given array
+    Finding the break-point, finding the next greater element, and reversal at the end takes O(N) for each, where N is the number of elements in the input array. This sums up to 3*O(N) which is approximately O(3N)
 
-    Space Complexity: 
+    Space Complexity: O(1), as we are not using any extra space to store the elements
 
     Reference: https://takeuforward.org/data-structure/next_permutation-find-next-lexicographically-greater-permutation/
 
@@ -35,18 +38,18 @@ public class Q3_optimal{
     public static List<Integer> nextGreaterPermutations(List<Integer> arr){
 
         // size of the array
-        int n = arr.size(); // 3 
+        int n = arr.size(); // 5
 
         // Step 1: Find the break point
-        int ind = -1; // 1
+        int ind = -1; 
         for(int i=n-2; i>=0; i--){
             if(arr.get(i) < arr.get(i+1)){
-                ind = i;
+                ind = i; // 1
                 break;
             }
         }
 
-        // if the break point does not exist 
+        // if the break point does not exist, it means that the permutation is the greatest permutation
         if(ind == -1){
             // reverse the whole array
             Collections.reverse(arr);
@@ -54,29 +57,28 @@ public class Q3_optimal{
 
 
         
-        // Step 2: Find the next greater element
-        // and swap it with arr[ind]
+        // Step 2: Find the next greater element and swap it with the breakpoint index you've got 
         for(int i=n-1; i>ind; i--){
             if(arr.get(i) > arr.get(ind)){ // 3 > 1
-                int temp = arr.get(i); // 3 
-                arr.set(i, arr.get(ind)); // 1 
-                arr.set(ind, temp); // 3 
-                break;
+                int temp = arr.get(i); // 3
+                arr.set(i, arr.get(ind)); // (index 4, 1)
+                arr.set(ind, temp); // (index 1, 3)
+                break; // 2 3 5 4 1
             }
         }
 
-        // 2 3 1 
-
         // step 3: Reverse the right half: 
+        // try to place the remaining elements in the sorted order 
         List<Integer> sublist = arr.subList(ind+1, n);
         Collections.reverse(sublist);
+        // 2 3 1 4 5 
 
         return arr;
     }
 
     public static void main(String[] args) {
         
-        List<Integer> A = Arrays.asList(new Integer[] {2,3,1,4});
+        List<Integer> A = Arrays.asList(new Integer[] {2,1,5,4,3});
         List<Integer> ans = nextGreaterPermutations(A);
         
 
