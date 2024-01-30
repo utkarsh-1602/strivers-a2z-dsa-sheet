4ee1445a4af0deb259fe78b898414d362ac812db
utkarsh-1602 <usking619@gmail.com>
2024-01-30 23:15:35 +0530

    Q6_optimal completed

---
+++ Arrays/Easy/Q6_optimal.java
@@ -0,0 +1,63 @@
+/*
+    Name : Rotate array by K elements
+
+    Problem Statement: Given an array of integers, rotating array of elements by k elements either left or right.
+
+    Input: N = 7, array[] = {1,2,3,4,5,6,7} , k=2 , right
+    Output: 6 7 1 2 3 4 5
+    Explanation: array is rotated to right by 2 position
+
+    Input: N = 6, array[] = {3,7,8,9,10,11} , k=3 , left 
+    Output: 9 10 11 3 7 8
+    Explanation: Array is rotated to right by 3 position.
+
+    Approach : Optimal 
+
+    Time Complexity: O(N)
+
+    Space Complexity: O(1)
+
+    Reference: https://takeuforward.org/data-structure/rotate-array-by-k-elements/
+
+ */
+public class Q6_optimal {
+
+    static void ReverseArray(int[] arr, int start, int end){
+
+        while(start <= end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void rotateByKElements(int[] arr, int k){
+        int n = arr.length;
+        int breakPoint = n-k;
+
+        // Reverse First n-k elements
+        ReverseArray(arr, 0, breakPoint-1);
+
+        // Reverse last k elements
+        ReverseArray(arr, breakPoint, n-1);
+
+        // Reverse Whole array
+        ReverseArray(arr, 0, n-1);
+
+
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int k = 2;
+        rotateByKElements(arr, k);
+    }
+}
