1deb33a101e3698bda9bff8b878f69721181214b
utkarsh-1602 <usking619@gmail.com>
2024-02-02 07:40:52 +0530

    Q10_optimal completed

---
+++ Arrays/Easy/Q10_optimal.java
@@ -0,0 +1,42 @@
+/*
+    Name : Count Maximum Consecutive One’s in the array
+
+    Problem Statement: Given an array that contains only 1 and 0 return the count of maximum consecutive ones in the array.
+
+    Input: prices = {1, 1, 0, 1, 1, 1}
+    Output: 3
+    Explanation: There are two consecutive 1’s and three consecutive 1’s in the array out of which maximum is 3.
+
+
+    Approach : optimal
+
+    Time Complexity: O(N)
+
+    Space Complexity: O(1)
+
+    Reference: https://takeuforward.org/data-structure/count-maximum-consecutive-ones-in-the-array/
+
+ */ 
+
+public class Q10_optimal {
+
+    static int maxConsecutiveOnes(int[] arr){
+
+        int maxCount = 0;
+        for(int i=0; i<arr.length; i++){
+            maxCount++;
+            if(arr[i] == 0){
+                maxCount = 0;
+            }
+        }
+
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr = {1,1,0,1,1,1};
+        int ans = maxConsecutiveOnes(arr);
+        System.out.println(ans);
+    }    
+}
