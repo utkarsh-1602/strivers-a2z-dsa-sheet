ffcba1315ea3cd89b5077dc55c705039b5153901
utkarsh-1602 <usking619@gmail.com>
2024-02-13 19:52:25 +0530

    quick_sort algorithm

---
+++ sorting_Algorithms/quick_sort/Q1.java
@@ -0,0 +1,41 @@
+package sorting_Algorithms.quick_sort;
+
+/*
+    Name : Merge Sort Algorithm
+
+    Problem Statement: Given an array of size n, sort the array using Merge Sort.
+
+    Example 1:
+    Input:  N = 5  , Arr[] = {4,1,7,9,3}
+    Output: 1 3 4 7 9 
+
+    Explanation: After sorting the array becomes 1, 3, 4, 7, 9
+
+    Example 2:
+    Input: N = 8 , Arr[] = {4,6,2,5,7,9,1,3}
+    Output: 1 2 3 4 5 6 7 9
+    Explanation: After sorting the array becomes 1, 3, 4, 7, 9
+
+    Time complexity:
+
+    Space Complexity: 
+
+    Reference: https://takeuforward.org/data-structure/merge-sort-algorithm/
+
+ */
+
+public class Q1{
+
+    static void quickSort(int[] arr){
+
+        int n = arr.length;
+
+
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr = {4,6,2,5,7,9,1,3};
+        quickSort(arr);
+    }
+}
