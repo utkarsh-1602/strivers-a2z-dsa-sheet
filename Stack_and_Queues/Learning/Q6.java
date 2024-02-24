63063ac31ed5210a4f71e17a23585686e8bf4d40
utkarsh-1602 <usking619@gmail.com>
2024-02-24 15:49:38 +0530

    Stack using Linkedlist introduction

---
+++ Stack_and_Queues/Learning/Q6.java
@@ -0,0 +1,22 @@
+/*
+    Name : Implement stack using linked list
+
+    Problem Statement: Basic knowledge of stack and operations in the linked list.
+
+    But Why to use linked list ? 
+    Use linked list for the stack, when the size of the stack is not known in advance.
+
+    Approach: we will use head of linked list as top() of the stack 
+
+    Time Complexity: 
+    for adding node in the beginning of the linked list : O(1)
+    for removing the first node from the linked list : O(1)
+
+    Space Complexity: O(N)
+
+    Reference: https://takeuforward.org/data-structure/implement-queue-using-stack/
+ */
+
+ public class Q6 {
+    
+}
