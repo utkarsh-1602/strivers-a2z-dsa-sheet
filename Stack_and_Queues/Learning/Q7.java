7fee3dbd71354df11865e0e756dc95f5afa367dc
utkarsh-1602 <usking619@gmail.com>
2024-02-27 00:24:01 +0530

    Implement Queue using linkedlist

--- Stack_and_Queues/Learning/Q7.java
+++ Stack_and_Queues/Learning/Q7.java
@@ -22,6 +22,82 @@
  */
 
 
-public class Q7 {
+class QueueNode{
+    int data;
+    QueueNode next;
+    QueueNode(int data){
+        this.data = data;
+        next = null; 
+    }
+ }
+
+public class Q7{
     
+    static class Queue{
+    QueueNode Front = null;
+    QueueNode Rear = null;
+    int size = 0;
+
+    boolean empty(){
+        return Front == null;
+    }
+
+    int Peek(){
+
+        if(empty()){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        else{
+            return Front.data;
+        }
+    }
+
+    void Enqueue(int data){
+
+        QueueNode temp = new QueueNode(data);
+        
+        if(temp == null){
+            System.out.println("Queue is Full");
+        }
+        else{
+            if(Front == null){
+                Front = temp;
+                Rear = temp;
+            }
+            else{
+                Rear.next = temp;
+                Rear = temp;
+            }
+        }
+        System.out.println(data + " Enqueued into queue");
+        size++;
+    }
+
+    void Dequeue(){
+        if(Front == null){
+            System.out.println("Queue is Empty");
+        }
+        else{
+            System.out.println(Front.data + " Dequeued from queue");
+            QueueNode temp = Front;
+            Front = Front.next;
+            size--;
+        }
+    }
+
+}
+    public static void main(String[] args) {
+        
+        Queue Q = new Queue();
+        Q.Enqueue(10);
+        Q.Enqueue(20);
+        Q.Enqueue(30);
+        Q.Enqueue(40);
+        Q.Enqueue(50);
+        Q.Dequeue();
+        System.out.println("The size of the Queue is "+Q.size);
+        System.out.println("The Peek element of the Queue is "+Q.Peek());
+
+    }   
 }
