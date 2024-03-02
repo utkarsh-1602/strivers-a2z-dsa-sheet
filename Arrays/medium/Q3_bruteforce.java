50cf2fad7cbcf3d8f0ddde9ee33b23a5fc35e5f6
utkarsh-1602 <usking619@gmail.com>
2024-03-02 21:40:09 +0530

    majority element occurs more that N/2, Q3_bruteforce

---
+++ Arrays/medium/Q3_bruteforce.java
@@ -0,0 +1,66 @@
+/*
+    Name: Find the Majority Element that occurs more than N/2 times.
+    
+    Problem Statement : Given an array of N integers, write a program to return an element that occurs more than N/2 times in the given array. You may consider that such an element always exists in the array.
+
+    Example 1:
+    Input Format: N = 3, nums[] = {3,2,3}
+    Result: 3
+    Explanation: When we just count the occurrences of each number and compare with half of the size of the array, you will get 3 for the above solution. 
+
+    Example 2:
+    Input Format:  N = 7, nums[] = {2,2,1,1,1,2,2}
+    Result: 2
+    Explanation: After counting the number of times each element appears and comparing it with half of array size, we get 2 as result.
+
+    Example 3:
+    Input Format:  N = 10, nums[] = {4,4,2,4,3,4,4,3,2,4}
+    Result: 4
+
+    Approach : bruteforce 
+
+    Time Complexity: O(N^2)
+    Space Complexity: O(1)
+
+    Reference: https://takeuforward.org/data-structure/find-the-majority-element-that-occurs-more-than-n-2-times/
+
+
+ */
+
+
+package medium;
+
+public class Q3_bruteforce{
+
+    static int occurrence(int[] arr){
+
+        int n = arr.length;
+        int occurrence = n/2;
+        int count = 0;
+
+        for(int i=0; i<arr.length; i++){
+
+            count = 0;
+            for(int j=0; j<arr.length; j++){
+                
+                if(arr[j] == arr[i]){
+                    count++;
+                }
+            }
+
+            if(count > occurrence){
+                return arr[i];
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr = {2,2,3,2,1,2,3};
+        int ans = occurrence(arr);
+
+        System.out.println(ans);
+    }
+}
