48f1306aaa1499e7893d7afd1cace144ce934ea7
utkarsh-1602 <usking619@gmail.com>
2024-03-16 02:09:50 +0530

    heap sort completed

--- sorting_Algorithms/heap_sort/Q1.java
+++ sorting_Algorithms/heap_sort/Q1.java
@@ -17,6 +17,11 @@ package sorting_Algorithms.heap_sort;
 
     Approach: Optimal 
 
+    1. build a complete binary tree from the given array
+    2. trasform that tree into max heap 
+    3. perform heap sort by swapping the max node to the lowest leaf node, and then deleting that max node 
+    4. return the sorted array 
+
     Time complexity: 
     Space Complexity: 
     
@@ -28,8 +33,84 @@ package sorting_Algorithms.heap_sort;
 public class Q1 {
 
 
+    // using heapify function, we are making the max heap 
+    static void heapify(int[] arr, int n, int i){
+
+         // Initialize largest as root  
+        int largest = i;
+
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if(left < n && arr[left] > arr[largest]){
+            largest = left;
+        }
+
+        if(right < n && arr[right] > arr[largest]){
+            largest = right;
+        }
+
+        if(largest != i){
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
+        }
+
+
+
+    }
+
+    static void heapSort(int[] arr){
+
+        // For the array {13, 46, 24, 52, 20, 9}, the binary tree would look like this:
+
+        /* 
+
+                 13
+                /  \
+               46   24
+              / \   /
+            52  20  9
+
+        */
+
+        // This tree represents the initial state of the array before performing the max heap transformation. It's worth noting that the binary tree's elements are arranged in the array in level-order traversal.
+
+        int n = arr.length;
+
+        // By starting the loop at n / 2 - 1, we're essentially starting from the last non-leaf node in the heap
+        // This is because elements beyond n / 2 - 1 are leaf nodes, and they don't have any children to heapify.
+        for(int i = n/2-1; i>=0; i--){
+            heapify(arr, n, i); 
+        }
+
+        // after getting the max heap 
+        // we will extract the elements one by one from the heap 
+
+        for(int i=n-1; i>=0; i--){
+            // Move current root element to end  
+            // swap a[0] with a[i]
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // after swapping, we got the last element at the root, so because of that we need to again make it max heap using heapify()
+            heapify(arr, i, 0);
+        }
+
+    }
+
+
     public static void main(String[] args) {
         
         int arr[] = {13,46,24,52,20,9};
+
+        heapSort(arr); 
+
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
