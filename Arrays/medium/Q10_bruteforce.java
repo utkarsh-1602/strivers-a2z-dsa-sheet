b17786a366a5bb2e471c0ec809198c9e2381fceb
utkarsh-1602 <usking619@gmail.com>
2024-04-29 08:08:08 +0530

    Set Matrix Zero - bruteforce

--- Arrays/medium/Q10_bruteforce.java
+++ Arrays/medium/Q10_bruteforce.java
@@ -1,3 +1,4 @@
+package medium;
 /*
     Name : Set Matrix Zero
 
@@ -27,7 +28,7 @@
 
 import java.util.*;
 
-public class Q1_bruteforce{
+public class Q10_bruteforce{
 
     static void markRow(ArrayList<ArrayList<Integer>> matrix, int n, int m, int i){
         
@@ -51,7 +52,7 @@ public class Q1_bruteforce{
     }
 
 
-    static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n, int m){
+    static ArrayList<ArrayList<Integer>> setMatrixZero(ArrayList<ArrayList<Integer>> matrix, int n, int m){
 
         // Set -1 for rows and cols that contains 0. Don't mark any 0 as -1
         for(int i=0; i<n; i++){
@@ -62,6 +63,8 @@ public class Q1_bruteforce{
                 }
             }
         }
+
+        printMatrix(matrix);
 
         // Finally, mark all -1 as 0:
         for(int i=0; i<n; i++){
@@ -76,23 +79,8 @@ public class Q1_bruteforce{
         
     }
 
-    public static void main(String[] args){
-        System.err.println("Hello world!");
-
-        // The code snippet you provided declares an ArrayList named matrix, where each element in the ArrayList is itself an ArrayList of integers
-        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+    static void printMatrix(ArrayList<ArrayList<Integer>> answer){
         
-        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
-        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
-        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
-
-        int n = matrix.size(); // number of rows
-        int m = matrix.get(0).size(); // number of columns
-
-        ArrayList<ArrayList<Integer>> answer = zeroMatrix(matrix, n, m);
-
-        System.out.println("Final matrix is : ");
-
         // This is an enhanced for loop that iterates over each row in the answer matrix. For each iteration, the current row is assigned to the variable row, which has the type ArrayList<Integer>.
         // Inside the outer loop, there is another enhanced for loop that iterates over each element (Integer x) in the current row (row).
         for(ArrayList<Integer> row: answer){
@@ -103,5 +91,30 @@ public class Q1_bruteforce{
         }
 
         // So, the nested for loop is responsible for iterating through each element in the 2D ArrayList answer and printing it to the console in a row-by-row fashion. The outer loop iterates through rows, and the inner loop iterates through elements in each row.
+    }
+
+    public static void main(String[] args){
+
+        // The code snippet you provided declares an ArrayList named matrix, where each element in the ArrayList is itself an ArrayList of integers
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+
+        printMatrix(matrix);
+
+        int n = matrix.size(); // number of rows
+        System.out.println("Matrix Rows size: " + n);
+
+        int m = matrix.get(0).size(); // number of columns
+        System.out.println("Matrix Columns size: " + n);
+
+        ArrayList<ArrayList<Integer>> answer = setMatrixZero(matrix, n, m);
+
+        System.out.println("Final matrix is : ");
+
+        printMatrix(answer);
+
     }
 }
