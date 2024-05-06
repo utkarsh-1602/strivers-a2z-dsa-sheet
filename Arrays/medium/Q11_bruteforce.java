09c338310cd9029177604213552d7c76d3edd2d0
utkarsh-1602 <usking619@gmail.com>
2024-05-06 11:57:33 +0530

    Q11_bruteforce - starting

---
+++ Arrays/medium/Q11_bruteforce.java
@@ -0,0 +1,28 @@
+package medium;
+/*
+    Name : Rotate Image by 90 degree
+
+    Problem Statement: Given a matrix, your task is to rotate the matrix 90 degrees clockwise.
+
+    Input: matrix= [[1,2,3],[4,5,6],[7,8,9]]
+
+    Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+    Explanation: Rotate the matrix simply by 90 degree clockwise and return the matrix.
+
+    Approach : bruteforce 
+
+    Time Complexity:
+    Space Complexity: 
+
+    Reference: https://takeuforward.org/data-structure/rotate-image-by-90-degree/
+
+ */
+
+public class Q11_bruteforce {
+
+   
+    public static void main(String[] args) {
+
+    }   
+}
